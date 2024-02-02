32852e18af69fc371d6b4ee336eb6b08c9e5817f
Lakshmi K Anilkumar <lakshmikanil2000@gmail.com>
2024-02-02 16:21:39 +0530

    changes

--- src/app/proof-upload/proof-upload.component.ts
+++ src/app/proof-upload/proof-upload.component.ts
@@ -8,10 +8,10 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './proof-upload.component.html',
   styleUrls: ['./proof-upload.component.css']
 })
-export class ProofUploadComponent implements OnInit{
-  AadharImage: string="";
-PANImage: string=""; 
-BankImage: string=""; 
+export class ProofUploadComponent implements OnInit {
+  // AadharImage: string = "";
+  // PANImage: string = "";
+  // BankImage: string = "";
   uploadForm: FormGroup;
 
   constructor(private fb: FormBuilder, private http: HttpClient) {
@@ -41,14 +41,14 @@ BankImage: string="";
   // onFileChange(event: any, type: string) {
   //   if (event.target.files.length > 0) {
   //     const file = event.target.files[0];
-      
+
   //     // Example: Assuming you want to store the file content as a base64 string
   //     this.getBase64(file).then((base64String: string) => {
   //       this.uploadForm.get(type).patchValue(base64String);
   //     });
   //   }
   // }
-  
+
   // // Helper method to convert a file to a base64-encoded string
   // getBase64(file: any): Promise<string> {
   //   return new Promise((resolve, reject) => {
@@ -59,22 +59,22 @@ BankImage: string="";
   //   });
   // }
 
-onSubmit() {
-  if(this.uploadForm.valid){
+  onSubmit() {
+    console.log("submit123")
+    // if(this.uploadForm.valid){
     // let userId = this.uploadForm.value.userId;
-    let userData={
-      aadharimg:this.uploadForm.value.AadharImage,
-      panimg:this.uploadForm.value.panImg,
-      bankimg:this.uploadForm.value.bankImg,
+    let userData = {
+      aadharimg: this.uploadForm.value.aadharImg,
+      panimg: this.uploadForm.value.panImg,
+      bankimg: this.uploadForm.value.bankImg,
     };
-    console.log('Submitted userData:',userData);
-    this.http.post("http://localhost:8080/api/fileuploads", userData, { responseType: 'text' }).subscribe((resultData:any)=>
-    {
-      console.log(resultData,"resultData");
-        localStorage.setItem('userID', resultData);
-        alert("bank details entered successfully");
+    console.log('Submitted userData:', userData);
+    this.http.post("http://localhost:8080/api/fileuploads", userData, { responseType: 'text' }).subscribe((resultData: any) => {
+      console.log(resultData, "resultData");
+      localStorage.setItem('userID', resultData);
+      alert("bank details entered successfully");
     });
-    
-    }
+
+    // }
   }
 }
