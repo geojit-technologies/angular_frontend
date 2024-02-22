f7a5f08cd1d8745e80534e4465bb6291913a3eab
GayathrySunil <122028291+GayathrySunil@users.noreply.github.com>
2024-02-22 12:09:56 +0530

    updation done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,13 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
@@ -43,7 +50,15 @@ import { MatIconModule } from '@angular/material/icon';
     FormsModule,
     MatInputModule,
     MatButtonModule,
-    MatIconModule 
+    MatIconModule ,
+    MatDatepickerModule,
+    MatInputModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    BrowserAnimationsModule
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
